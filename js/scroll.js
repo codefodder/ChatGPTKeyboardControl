ccd4f231e77b9796993257cee86b5a83b2db0c97
Jason Milkins <jasonm23@gmail.com>
2023-05-16 15:17:26 +0700

    Get history working a bit better, still not ideal.
    Better than nothing :D

--- js/scroll.js
+++ js/scroll.js
@@ -1,3 +1,7 @@
+// debug_print = console.log
+// disable debug_print
+debug_print = (t) => {}
+
 function floorEvenInt(index){ return (index % 2 == 0) ? index : index - 1 }
 function maxQuestion(){ return document.querySelectorAll('.items-center .group').length - 1 }
 function keyScroller(){ return document.querySelector('main div.flex-1 div div') }
@@ -7,56 +11,87 @@ let largeOffset = 500
 let smallOffset = 100
 
 var historyIndex = maxQuestion()
+var currentInput = ""
+
+function updateTextInput(text) {
+  textInput().value = text
+  // Need to figure out how the page does resize on the textarea...
+  // .. it's not this though.
+  // textInput().dispatchEvent(new Event('keypress', {keyCode: 300, key: " "}))
+}
 
 function textOfQuestion(index) {
+  debug_print(`textOfQuestion(${index})`)
   // wrap-around
-  i = index > maxQuestion() ? 0 : index
+  var i = index > maxQuestion() ? 0 : index
   i = index < 0 ? maxQuestion() : index
 
   if (index != i) {
     return [
       i,
-      textInput().value
+      currentInput
     ]
   }
 
   i = floorEvenInt(index)
+  var text = ""
+
+  try {
+    text = document.querySelectorAll('.items-center .group')[i].innerText
+  } catch(e) {
+    debug_print(e)
+    i = maxQuestion()
+    text = currentInput
+  }
+
+  debug_print(`Max Question: ${maxQuestion()}`)
+  debug_print(`Question index: ${i}`)
+  debug_print(`Question Text: ${text}`)
+  debug_print(`Current Input: ${currentInput}`)
+
   return [
-    i,
-    document.
-      querySelectorAll('.items-center .group')[
-        floorEvenInt(i)
-      ].innerText
+    i, text
   ]
 }
 
 window.addEventListener('keyup', function(e){
+  if (e.detail?.ignore) {
+    return
+  }
 
   if (e.key == 'Enter') {
     textInput().focus()
   }
   else if (e.altKey && e.ctrlKey && e.key == 'ArrowUp') {
     keyScroller().scrollBy(0, -smallOffset)
+    return
   }
   else if (e.altKey  && e.key == 'ArrowUp') {
     let [i, text] = textOfQuestion(historyIndex - 2)
     historyIndex = i
-    textInput().value = text
+    updateTextInput(text)
+    return
   }
   else if (e.ctrlKey && e.key == 'ArrowUp') {
     keyScroller().scrollBy(0, -largeOffset)
+    return
   }
   else if (e.altKey  && e.key == 'ArrowDown') {
     let [i, text] = textOfQuestion(historyIndex + 2)
     historyIndex = i
-    textInput().value = text
+    updateTextInput(text)
+    return
   }
   else if (e.altKey && e.ctrlKey && e.key == 'ArrowDown') {
     keyScroller().scrollBy(0, smallOffset)
+    return
   }
   else if (e.ctrlKey && e.key == 'ArrowDown') {
     keyScroller().scrollBy(0, largeOffset)
+    return
   }
+
+  currentInput = textInput().value
 })
 
-console.log("Loaded chatGPTKeyboarControl")
+debug_print("Loaded chatGPTKeyboarControl")
